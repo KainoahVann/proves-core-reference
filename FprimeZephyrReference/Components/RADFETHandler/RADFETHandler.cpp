8139d0e1776189134d396aeb446b111c6c955a2b
Gabren Sysavath <145822520+gsysavath@users.noreply.github.com>
2025-11-30 21:51:02 -0800

    changed the data packet being sent to gds as an event

--- FprimeZephyrReference/Components/RADFETHandler/RADFETHandler.cpp
+++ FprimeZephyrReference/Components/RADFETHandler/RADFETHandler.cpp
@@ -43,7 +43,8 @@ void RADFETHandler ::dataIn_handler(FwIndexType portNum, Fw::Buffer& buffer, con
     U32 dataSize = static_cast<U32>(buffer.getSize());
 
     if(dataSize >= 4){
-        this->log_ACTIVITY_LO_RawDataReceived(data[0]);
+        this->log_ACTIVITY_LO_RawDataReceived(data[3]);
+        this->log_ACTIVITY_LO_RawDataReceived(data[4]);
     }
 
     if (!accumulateSensorData(data, dataSize)){
